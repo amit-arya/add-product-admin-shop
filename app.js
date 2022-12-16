58338e4f89eea46067619d722e31705056d403ba
amit-arya <amit3128arya@gmail.com>
2022-12-16 16:29:27 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const path =require('path');
+const path = require('path');
 const express = require("express");
 const bodyParser = require('body-parser');
 
@@ -7,19 +7,18 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const contactus = require('./routes/contactus');
+const errorController = require('./controllers/NotFound');
+const contactController = require('./controllers/contact_succ');
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(contactus);
 
-app.use('/success',(req,res,next) => {
-  res.status(404).send("Form filled successfully");
-});
+app.use('/success',contactController.success);
 
-app.use((req,res,next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', 'NotFound.html'));
-})
+app.use(errorController.get404);
 
-app.listen(4000);
+app.listen(3000);
